577d8b0b9763ffdde964c97522e593dcec7a680e
Bry Cox <carl.cc@gmail.com>
2018-12-31 18:35:14 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,7 @@ def position_taken?(board, index)
 if board[index] != "X" && board[index] != "O"
       return false
     else
-    board[index] == "X" && board[index] == "O" 
+    board[index] == "X" && board[index] == "O"
       return true
     end
   end
