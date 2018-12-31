13185588e689cc23c88f475c39f5e5924262ede0
Bry Cox <carl.cc@gmail.com>
2018-12-31 18:31:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@ def valid_move?(board, index)
    if position_taken?(board, index) == false
       return true
    else
-      position_taken?(board, index) == true && index.between?(0, 8)
+      position_taken?(board, index) == true 
       return false
   end
   end
@@ -11,7 +11,7 @@ def position_taken?(board, index)
 if board[index] != "X" && board[index] != "O"
       return false
     else
-    board[index] == "X" || board[index] == "O"
+    board[index] == "X" && board[index] == "O" && index.between?(0, 8)
       return true
     end
   end
