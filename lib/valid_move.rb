9bb049f80cebc66be89179876d81f132b1c2d864
Bry Cox <carl.cc@gmail.com>
2018-12-31 18:31:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@ def valid_move?(board, index)
    if position_taken?(board, index) == false
       return true
    else
-      position_taken?(board, index) == true 
+      position_taken?(board, index) == true
       return false
   end
   end
